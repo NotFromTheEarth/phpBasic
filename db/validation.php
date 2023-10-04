461755bea4a9ab99d6ea4a79d6cf3fe38bd595f1
Thaiguer Krug <thaiguer.k@gmail.com>
2023-10-03 21:39:40 -0300

    add validation file

---
+++ db/validation.php
@@ -0,0 +1,16 @@
+<?php
+    function is_valid_user_name($username)
+    {
+        return true;
+    }
+
+    function is_valid_password($password)
+    {
+        return true;
+    }
+
+    function is_valid_nickname($nickname)
+    {
+        return true;
+    }
+?>
